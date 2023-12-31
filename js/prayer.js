b2e316bb03459f96ea18b2e9cd9ed8337f6b9b52
Sam-Wisdoms Amenyenu <sampson.aglanu@gmail.com>
2023-12-31 20:21:33 +0000

    Added Some Prayers Using Vanilla JS

---
+++ js/prayer.js
@@ -0,0 +1,43 @@
+const friday291223Prayer = document.createElement("div")
+
+const prayerScripture291223 = document.createElement("h2");
+prayerScriptureText = document.createTextNode("Behold, they shall surely gather together, but not by me: whosoever shall gather together against thee shall fall for thy sake. (Isaiah 54:15)")
+prayerScripture291223.appendChild(prayerScriptureText)
+
+const purpose291223 = document.createElement("h1")
+purposeText = document.createTextNode("PRAYER")
+purpose291223.appendChild(purposeText)
+
+const parag291223 = document.createElement("h4")
+paragText = document.createTextNode("Let them fall for our sake O Lord, them that have gathered together against Your children and good works. This I declare today O Father, cause every gather together against me, my family, my friends, and my loved ones  backfire. Let any wicked man or woman, walking either in the dark or in light against Your children stumble, surrender and confess their evil deeds. And let any evil priest, that have received any form of assignment against Your children die in the name of Jesus. Let every perpetrator of evil and wickedness encounter Your wrath O Lord, that they may know that there is a God in heaven, the One in whom we Your children we put our Hope and trust for redemption. Deliver us O Lord in Jesus Mighty Name, Amen.")
+parag291223.appendChild(paragText)
+
+friday291223Prayer.appendChild(prayerScripture291223)
+friday291223Prayer.appendChild(purpose291223)
+friday291223Prayer.appendChild(parag291223)
+
+document.body.appendChild(friday291223Prayer)
+
+
+
+
+const saturday30122023Prayer = document.createElement('div')
+
+const prayerScripture301223 = document.createElement('h2')
+prayerScriptureText = document.createTextNode("We escaped like a bird from a hunter's trap. The trap is broken, and we are free! (Psalm 124:7)");
+prayerScripture301223.appendChild(prayerScriptureText)
+
+const purpose = document.createElement('h1')
+purposeText = document.createTextNode("PRAYER");
+purpose.style.justifyContent = 'center' //how can I center and display the text in a card and wrap it?
+purpose.appendChild(purposeText)
+
+const parag = document.createElement('h4')
+paragText = document.createTextNode("Thank You, gracious Father for preserving and delivering us from the hands of evil men. For many are them that wished and worked for our fall in the year but you delivered us from their traps all. Advance us higher in the coming years, and continue to preserve and protect us at all times from the reach of evil in Jesus Mighty Name, Amen.");
+parag.appendChild(paragText)
+
+saturday30122023Prayer.appendChild(prayerScripture301223)
+saturday30122023Prayer.appendChild(purpose)
+saturday30122023Prayer.appendChild(parag) 
+
+document.body.appendChild(saturday30122023Prayer)
